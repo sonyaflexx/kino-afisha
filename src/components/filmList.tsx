02cdeee32bccfa03a1130cb36f641cbf61757433
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-04-22 08:14:37 +0700

    Add getPopularMovies

--- src/components/filmList.tsx
+++ src/components/filmList.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import FilmItem from "./filmItem";
 import { GetServerSideProps } from 'next';
+import getPopularMovies from "@/lib/getPopularMovies";
 
 interface Movie {
     id: number;
@@ -11,38 +12,17 @@ interface Movie {
     movies: Movie[];
   }
 
-const FilmList: React.FC<MoviesProps> = ({ movies }) => {
+const FilmList = () => {
+    const movies = getPopularMovies();
+
     return (
         <section className="col-span-4">
           <h1 className="font-semibold text-2xl pb-5">Список фильмов</h1>
           <ul className="flex flex-col gap-4">
-            <FilmItem />
-            { movies.map(movie => (
-                <FilmItem key={movie.id} title={movie.title} />
-            )) }
+            
           </ul>
         </section>
     );
 };
-
-export const getServerSideProps: GetServerSideProps<MoviesProps> = async () => {
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTRhODIwZGQ1MDc2MzI4MWMyZThjZjJiZmEwMmM0MSIsInN1YiI6IjY2MjU2YzEyMjIxYmE2MDE3YzE0YTcxNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j8ZvovR7C-_DEwbqI087EaAU-JcpMkL96cQeKYph0C8'
-      }
-    };
-  
-    const response = await fetch('https://api.themoviedb.org/3/movie/popular?language=ru-RU&page=1', options);
-    const data = await response.json();
-    const movies: Movie[] = data.results;
-  
-    return {
-      props: {
-        movies,
-      },
-    };
-  };
 
 export default FilmList;
--- src/lib/getPopularMovies.ts
+++ src/lib/getPopularMovies.ts
@@ -1,12 +1,20 @@
-const options = {
+const getPopularMovies = async () => {
+  const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTRhODIwZGQ1MDc2MzI4MWMyZThjZjJiZmEwMmM0MSIsInN1YiI6IjY2MjU2YzEyMjIxYmE2MDE3YzE0YTcxNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j8ZvovR7C-_DEwbqI087EaAU-JcpMkL96cQeKYph0C8'
+      Authorization: `Bearer ${process.env.API_KEY}`
     }
   };
+
+  const res = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
   
-  fetch('https://api.themoviedb.org/3/movie/popular?language=ru-RU&page=1', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  console.log(res)
+  return res.json();
+}
+
+export default getPopularMovies;
