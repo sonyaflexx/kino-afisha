10e4a1abdf7675c9d17f87e571197942ff8f4a07
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-04-22 08:23:03 +0700

    update getPopularMovie

--- src/components/filmList.tsx
+++ src/components/filmList.tsx
@@ -1,27 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import FilmItem from "./filmItem";
-import { GetServerSideProps } from 'next';
 import getPopularMovies from "@/lib/getPopularMovies";
 
 interface Movie {
     id: number;
     title: string;
-  }
+}
   
-  interface MoviesProps {
+interface MoviesProps {
     movies: Movie[];
-  }
+}
 
 const FilmList = () => {
-    const movies = getPopularMovies();
-    console.log(movies)
+    const [movies, setMovies] = useState<Movie[]>([]);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const response = await getPopularMovies();
+                setMovies(response.results);
+            } catch (error) {
+                console.error("Failed to fetch movies:", error);
+            }
+        };
+        fetchMovies();
+    }, []);
 
     return (
         <section className="col-span-4">
-          <h1 className="font-semibold text-2xl pb-5">Список фильмов</h1>
-          <ul className="flex flex-col gap-4">
-            
-          </ul>
+            <h1 className="font-semibold text-2xl pb-5">Список фильмов</h1>
+            <ul className="flex flex-col gap-4">
+                {movies.map(movie => (
+                    <FilmItem id={movie.id} title={movie.title} />
+                ))}
+            </ul>
         </section>
     );
 };
